905ff667a76a947096fc86564e4ce262b0ff1ec3
mcjcc <mcjohnnychen@gmail.com>
2017-11-07 12:31:25 -0800

    Have more progress done

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,5 +1,6 @@
 const models = require('../models');
 const Promise = require('bluebird');
+const parseCookies = require('./cookieParser');
 
 module.exports.createSession = (req, res, next) => {
 
@@ -9,19 +10,46 @@ module.exports.createSession = (req, res, next) => {
   // associate the logged in user to the session record
 
   // var session = 
+  
+  //checks if request already has a cookie. if it does, it retrieves the session
+  parseCookies(req, res, function() {
+    // if cookie exist , check db
+    if (req.cookies.shortlyid) {
+      // we can assume req.cookies.shortlyid is the session hash
+      // check database to see if a session matchs req.cookies.shortlyid
+      console.log(req);
+      var hash = req.cookies.shortlyid;
+      models.Sessions.get({hash: hash})
+        .then(result => {
+          console.log('THE SESSION AFTER CHECKING COOKIE');
+          // if result exists, we update the session record with the associated user id
+          // if result doesnt exist 
 
-  models.Sessions.create()
-    .then(results => {
-      return models.Sessions.get({id: results.insertId});
-    }).then(result => {
-      req.session = {hash: result.hash};
-      res.cookies = {};      
-      res.cookies['shortlyid'] = {value: result.hash};
-      next();
+        });
+    }
 
-    });
+    // if cookie doesnt exist, we do the creeate sessions stuff here
+    models.Sessions.create()
+      .then(results => {
+        return models.Sessions.get({id: results.insertId});
+      }).then(result => {
+        console.log('req', req);
+        req.session = {hash: result.hash};
+        res.cookies = {};     
+        // assign session to a user somehow
+        
+        // set cookies to the session hash;
+        res.cookies['shortlyid'] = {value: req.session.hash};
+        next();
+
+      });
+
+  });
   
 };
+
+// req.cookies = {shortlyid: '23532523'};
+// res.cookies = {shorltyid: {value: '2352352'}};
 
 /************************************************************/
 // Add additional authentication middleware functions below
@@ -33,3 +61,9 @@ module.exports.createSession = (req, res, next) => {
 
 
 
+// req.session = {
+//   userId: 4,
+//   user: {
+//     username: 'john'
+//   }
+// };
