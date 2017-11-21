4db4f51d79a5b431c0db8124542d23711456b769
Johnny Chen <mcjohnnychen@gmail.com>
2017-11-21 10:22:14 -0800

    Fix linter errors

--- server/app.js
+++ server/app.js
@@ -14,89 +14,89 @@ app.use(partials());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public')));
-//writing an app use that uses auth.js file as middleware. It will check req for session in order to save it, for all requests 
+//writing an app use that uses auth.js file as middleware. It will check req for session in order to save it, for all requests
 app.use(Auth.createSession);
 
 
 
 
-app.get('/', 
-(req, res) => {
-  if (models.Sessions.isLoggedIn(req.session)) {
-    res.render('index');  
-  } else {
-    res.redirect('/login');
-  }
-});
+app.get('/',
+  (req, res) => {
+    if (models.Sessions.isLoggedIn(req.session)) {
+      res.render('index');
+    } else {
+      res.redirect('/login');
+    }
+  });
 
 app.get('/login',
-(req, res) => {
-  res.render('login');
-});
+  (req, res) => {
+    res.render('login');
+  });
 
-app.get('/create', 
-(req, res) => {
-  if (models.Sessions.isLoggedIn(req.session)) {
-    res.render('index');  
-  } else {
-    res.redirect('/login');
-  }
-});
-
-app.get('/links', 
-(req, res, next) => {
-  if (models.Sessions.isLoggedIn(req.session)) {
-    models.Links.getAll()
-    .then(links => {
-      res.status(200).send(links);
-    })
-    .error(error => {
-      res.status(500).send(error);
-    });
-  } else {
-    res.redirect('/login'); 
-  }
-});
-
-app.post('/links', 
-(req, res, next) => {
-  if (models.Sessions.isLoggedIn(req.session)) {
-    var url = req.body.url;
-    if (!models.Links.isValidUrl(url)) {
-      // send back a 404 if link is not valid
-      return res.sendStatus(404);
+app.get('/create',
+  (req, res) => {
+    if (models.Sessions.isLoggedIn(req.session)) {
+      res.render('index');
+    } else {
+      res.redirect('/login');
     }
+  });
 
-    return models.Links.get({ url })
-      .then(link => {
-        if (link) {
-          throw link;
-        }
-        return models.Links.getUrlTitle(url);
-      })
-      .then(title => {
-        return models.Links.create({
-          url: url,
-          title: title,
-          baseUrl: req.headers.origin
+app.get('/links',
+  (req, res, next) => {
+    if (models.Sessions.isLoggedIn(req.session)) {
+      models.Links.getAll()
+        .then(links => {
+          res.status(200).send(links);
+        })
+        .error(error => {
+          res.status(500).send(error);
         });
-      })
-      .then(results => {
-        return models.Links.get({ id: results.insertId });
-      })
-      .then(link => {
-        throw link;
-      })
-      .error(error => {
-        res.status(500).send(error);
-      })
-      .catch(link => {
-        res.status(200).send(link);
-      });
-  } else {
-    res.redirect('/login');
-  }
-});
+    } else {
+      res.redirect('/login');
+    }
+  });
+
+app.post('/links',
+  (req, res, next) => {
+    if (models.Sessions.isLoggedIn(req.session)) {
+      var url = req.body.url;
+      if (!models.Links.isValidUrl(url)) {
+        // send back a 404 if link is not valid
+        return res.sendStatus(404);
+      }
+
+      return models.Links.get({ url })
+        .then(link => {
+          if (link) {
+            throw link;
+          }
+          return models.Links.getUrlTitle(url);
+        })
+        .then(title => {
+          return models.Links.create({
+            url: url,
+            title: title,
+            baseUrl: req.headers.origin
+          });
+        })
+        .then(results => {
+          return models.Links.get({ id: results.insertId });
+        })
+        .then(link => {
+          throw link;
+        })
+        .error(error => {
+          res.status(500).send(error);
+        })
+        .catch(link => {
+          res.status(200).send(link);
+        });
+    } else {
+      res.redirect('/login');
+    }
+  });
 
 /************************************************************/
 // Write your authentication routes here
@@ -104,95 +104,93 @@ app.post('/links',
 app.get('/signup', (req, res, next) =>{
   res.render('signup');
 });
-app.post('/signup', 
+app.post('/signup',
   // if user alreayd exists, we dont create the user and respond with an error message saying the username is taken
   // if it deosnt exist, we create it!
   // res.status(201).send('Hello');
-  
-(req, res, next) => {
-// store the request body info in a variable
-// call models.Users.create with the user info as a param
-// on success respond with appropriate header/body
-  var username = req.body.username.toString();
-  var password = req.body.password.toString();
-  // console.log(req.query);
-  console.log('Username: ', username, 'Password ', password);
 
-  models.Users.create({username, password})
-    .then(results => {
-      console.log('user created!');
-      return models.Sessions.update({hash: req.session.hash}, {userId: results.insertId});
-    })
-    .then(result => {
-      
-      res.status(201).redirect('/');
-         
-    })
-    .error(error => {
-      console.log('REACHED THE ERROR BLOCK', error.code);
+  (req, res, next) => {
+  // store the request body info in a variable
+  // call models.Users.create with the user info as a param
+  // on success respond with appropriate header/body
+    var username = req.body.username.toString();
+    var password = req.body.password.toString();
+    // console.log(req.query);
+    console.log('Username: ', username, 'Password ', password);
 
-      if (error.code.toString() === 'ER_DUP_ENTRY') {
-        res.redirect('/signup');
-      }
-      // res.status(500).send(error);
-    })
-    .catch(user => {
-      res.status(200).send('it got to the catch 200');
-    });
-});
+    models.Users.create({username, password})
+      .then(results => {
+        console.log('user created!');
+        return models.Sessions.update({hash: req.session.hash}, {userId: results.insertId});
+      })
+      .then(result => {
+        res.status(201).redirect('/');
+      })
+      .error(error => {
+        console.log('REACHED THE ERROR BLOCK', error.code);
+
+        if (error.code.toString() === 'ER_DUP_ENTRY') {
+          res.redirect('/signup');
+        }
+        // res.status(500).send(error);
+      })
+      .catch(user => {
+        res.status(200).send('it got to the catch 200');
+      });
+  });
 
 app.post('/login',
 
-(req, res, next) => {
-  var username = req.body.username.toString();
-  var attemptedPassword = req.body.password.toString();
-  
-  models.Users.get({username})
-    .then(result => {
-      console.log('RETURN VALUE FROM DB QUERY', result);
-      if (result) {
-        var password = result.password;
-        var salt = result.salt;
-        var userId = result.id;
-        if ( models.Users.compare(attemptedPassword, password, salt) ) {
-          // WE HAVE TO FIND THE REQ.SESSIONS OBJECT HERE AND THEN UPDATE IT IN THE TABLES WITH THE USERID?
-  
-          
-          console.log('REQ SESSION FOR LOGGED IN', req.session);
-        
-          console.log('USER HAS VERIFIED PASSWORD, AND NEEDS SESSION TO HAVE USER INFO UPDATED');
-          // Auth.updateSessionWithUser(req, res, userId, function() {
-  
-          models.Sessions.update({hash: req.session.hash}, {userId: userId}).then(result => {
-            res.redirect('/');
-         
-          });
+  (req, res, next) => {
+    var username = req.body.username.toString();
+    var attemptedPassword = req.body.password.toString();
 
-          // }); 
+    models.Users.get({username})
+      .then(result => {
+        console.log('RETURN VALUE FROM DB QUERY', result);
+        if (result) {
+          var password = result.password;
+          var salt = result.salt;
+          var userId = result.id;
+          if ( models.Users.compare(attemptedPassword, password, salt) ) {
+            // WE HAVE TO FIND THE REQ.SESSIONS OBJECT HERE AND THEN UPDATE IT IN THE TABLES WITH THE USERID?
+
+
+            console.log('REQ SESSION FOR LOGGED IN', req.session);
+
+            console.log('USER HAS VERIFIED PASSWORD, AND NEEDS SESSION TO HAVE USER INFO UPDATED');
+            // Auth.updateSessionWithUser(req, res, userId, function() {
+
+            models.Sessions.update({hash: req.session.hash}, {userId: userId}).then(result => {
+              res.redirect('/');
+
+            });
+
+          // });
 
 
           // res.redirect('/');
-        } else { // password supplied is wrong
-          console.log('password not verified, redirecting to /login');
+          } else { // password supplied is wrong
+            console.log('password not verified, redirecting to /login');
+            res.redirect('/login');
+          }
+
+        } else { // if user wasnt found
+          console.log('USER WAS NOT FOUND!');
           res.redirect('/login');
         }
-            
-      } else { // if user wasnt found
-        console.log('USER WAS NOT FOUND!');
-        res.redirect('/login');
-      }
-    });
-/// take request info and search for the user in the user database
-// if user exists, check if passwords match
-  // if passwords match, redirect to '/',
-  // if passwords dont match, keep them at '/login' page
-// if user doesnt exist, keep them at '/login' page
-  
-}
+      });
+  /// take request info and search for the user in the user database
+  // if user exists, check if passwords match
+    // if passwords match, redirect to '/',
+    // if passwords dont match, keep them at '/login' page
+  // if user doesnt exist, keep them at '/login' page
+
+  }
 );
 
 app.get('/logout', (req, res, next) => {
-  
+
   res.cookie('shortlyid', null);
   models.Sessions.delete({hash: req.session.hash}).then( result => {
     console.log('INSIDE THE DELETE PROMISE', result);
